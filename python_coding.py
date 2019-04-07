82ffd34c1b73bba084ae7726ab64fa6265429d72
mbhagra <bhagrameeta@gmail.com>
2019-04-07 14:06:10 -0700

    Update python_coding.py

--- python_coding.py
+++ python_coding.py
@@ -975,5 +975,17 @@ Group anagrams of string togher
         return list(dict.values())
 								 
 ------------------------------------------------------------------------------------------------
+    Maximum Subarray
+Given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest sum and return its sum.
+
 								 
+ def maxSubArray(self, nums):
+        dp = [0] * len(nums)
+        dp[0] = nums[0]
+            # maxcurrent = gloabalcurrent = nums[0] 
+        for i in range(1, len(nums)):
+            dp[i] = max(nums[i] , dp[i-1] + nums[i] )
+        return max(dp)
+            								 
 								 
+-----------------------------------------------------------------------------------------------------------------
